a0a3d3fb9b88ffa193d5af4b79e763661c44cd43
Prashanth-naidu <saiprashanthnaidu68@gmail.com>
2024-06-23 15:37:48 +0530

    Update WriterJpaRepository.java

--- src/main/java/com/example/wordlyweek/repository/WriterJpaRepository.java
+++ src/main/java/com/example/wordlyweek/repository/WriterJpaRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 import com.example.wordlyweek.model.Writer;
 
 @Repository
-public interface WriterJpaRepository extends JpaRepository<Writer, Integer>{}
+public interface WriterJpaRepository extends JpaRepository<Writer, Integer> {
+}
